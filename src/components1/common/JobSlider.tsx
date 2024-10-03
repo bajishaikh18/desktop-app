377a313c36220990305241c241828b384722f431
HEMANTH SAI K <dev.wonderly@f1studioz.com>
2024-10-03 18:32:45 +0530

    change slider arrow

--- src/components1/common/JobSlider.tsx
+++ src/components1/common/JobSlider.tsx
@@ -17,8 +17,8 @@ const JobSlider: React.FC = () => {
     autoplay: true,
     autoplaySpeed: 3000,  // Automatically scroll every 3 seconds
     arrows: true,
-    //prevArrow: <button className="slider-arrow">❮</button>,
-   // nextArrow: <button className="slider-arrow">❯</button>,
+    prevArrow: <button className={`${styles.sliderArrow} slider-arrow`}>❮</button>,
+    nextArrow: <button className={`${styles.sliderArrow} slider-arrow`}>❯</button>,
   };
 
   // Submit search query connected to an API or search logic
@@ -32,13 +32,10 @@ const JobSlider: React.FC = () => {
       <Slider {...settings}>
         {/* First Slide */}
         <div>
-        <Image src="/slider.png" alt="Slider Image" fill={true}  
-            />
-
+          <Image src="/slider.png" alt="Slider Image" fill={true} />
           <div className={styles.unionImageContainer}>
             <Image src="/Union.png" alt="Union Icon" width={6.25} height={10} />
           </div>
-
           <div className={styles.overlay}>
             <h1 className={`${styles.textCommon} ${styles.title}`}>
               Apply International Job Openings across
@@ -46,16 +43,15 @@ const JobSlider: React.FC = () => {
             <h2 className={`${styles.textCommon} ${styles.subtitle}`}>
               Dubai, Qatar, Oman, Bahrain & Saudi Arabia
             </h2>
-          
-          <div className={styles.buttonRightContainer}>
-            <button className={styles.viewJobsButton} onClick={() => window.location.href = '/jobs'}>
-              View Job
-            </button>
+            <div className={styles.buttonRightContainer}>
+              <button className={styles.viewJobsButton} onClick={() => window.location.href = '/jobs'}>
+                View Job
+              </button>
+            </div>
+            <div className={styles.unionImageRightContainer}>
+              <Image src="/Union (1).png" alt="Union Right Icon" width={6.25} height={10} />
+            </div>
           </div>
-
-          <div className={styles.unionImageRightContainer}>
-            <Image src="/Union (1).png" alt="Union Right Icon" width={6.25} height={10} />
-          </div></div>
           <div className={styles.sliderLogoContainer}>
             <Image src="/sliderlogo.png" alt="Slider Logo" width={58} height={5} />
           </div>
@@ -70,8 +66,6 @@ const JobSlider: React.FC = () => {
             width={1440}
             height={190}
           />
-       
-       
         </div>
       </Slider>
 
