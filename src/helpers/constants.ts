898bea6c7e9a9d024bd247396397e3971e2795eb
Vipin <dev.wonderly@f1studioz.com>
2024-10-10 10:09:41 +0530

    changed base url

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'https://ec2-3-110-104-29.ap-south-1.compute.amazonaws.com:3030';
-export const BASE_URL = 'http://localhost:3030';
+export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const IMAGE_BASE_URL = "https://wonderlybackendpublic.s3.ap-south-1.amazonaws.com";
 
