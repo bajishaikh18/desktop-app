8f8e8b9771a38f2653a41fef54370a9f553d9da1
HEMANTH SAI K <dev.wonderly@f1studioz.com>
2024-12-09 17:16:29 +0530

    update number value

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,8 +1,6 @@
 // export const BASE_URL = 'https://ec2-3-110-104-29.ap-south-1.compute.amazonaws.com:3030';
 
 export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://ec2-65-1-25-159.ap-south-1.compute.amazonaws.com';
-
-
 export const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL ||"https://boon-backend-public-dev.s3.ap-south-1.amazonaws.com"
 
 export const COUNTRIES = {
