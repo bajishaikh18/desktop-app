b05d37b7a2c6e1c6a6425a342a11eea13cff1df0
HEMANTH SAI K <dev.wonderly@f1studioz.com>
2024-12-17 12:05:24 +0530

    update experience-feild

--- src/helpers/experince.ts
+++ src/helpers/experince.ts
@@ -1,8 +1,8 @@
-const maxYears = 10; 
-const rangeStep = 1; 
 
-
+const maxYears = 10;
+const rangeStep = 1;
 const experienceLabels: Record<string, string> = {};
+
 for (let i = 1; i <= maxYears; i++) {
   const start = i;
   const end = start + rangeStep;
@@ -12,9 +12,29 @@ for (let i = 1; i <= maxYears; i++) {
 
 experienceLabels[`${maxYears}`] = `${maxYears}+ years`;
 
-
 export const getExperienceLabel = (experience: string): string => {
-  return experienceLabels[experience] || `${experience} years`; 
+  return experienceLabels[experience] || `${experience} years`;
+};
+
+export const generateExperienceRanges = (rangeStep: number, steps: number): { value: string; label: string }[] => {
+  const experienceRanges: { value: string; label: string }[] = [];
+
+  for (let i = 0; i < steps; i++) {
+    if (i < steps - 1) {
+      experienceRanges.push({
+        value: `${i}`,
+        label: `${i}-${i + rangeStep} Years`,
+      });
+    } else {
+      experienceRanges.push({
+        value: `${i}`,
+        label: `${i}+ Years`,
+      });
+    }
+  }
+
+  return experienceRanges;
 };
 
 
+export const yearsOfExperience = generateExperienceRanges(rangeStep, maxYears + 1);
