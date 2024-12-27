31734b77a5681c03edb27e1da14586cfa299c027
HEMANTH SAI K <dev.wonderly@f1studioz.com>
2024-12-27 13:21:01 +0530

    infinite scrolling

--- src/components/registeredAgencies/AgencyJobPostings.tsx
+++ src/components/registeredAgencies/AgencyJobPostings.tsx
@@ -1,18 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
-
+"use client";
+import React, { useState } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Loader, NotFound } from "../common/Feedbacks";
 import agencyStyles from "./AgencyDetails.module.scss";
 import { Col, Row } from "react-bootstrap";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
-import {
-  COUNTRIES,
-  FACILITIES_IMAGES,
-  IMAGE_BASE_URL,
-} from "@/helpers/constants";
+import { COUNTRIES, FACILITIES_IMAGES, IMAGE_BASE_URL } from "@/helpers/constants";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { AiOutlineInfoCircle } from "react-icons/ai";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { getJobs } from "@/apis/jobs";
 
 type AgencyJobPostingsType = {
@@ -21,6 +19,14 @@ type AgencyJobPostingsType = {
 };
 
 type TabType = "active" | "expired";
+
+type Job = {
+  _id: string;
+  imageUrl?: string;
+  jobId: string;
+  location: string;
+  amenities: string[];
+};
 
 const JobCard = ({
   type,
@@ -33,30 +39,42 @@ const JobCard = ({
 }) => {
   const router = useRouter();
   const t = useTranslations("AgencyPostings");
-  const { data, isLoading, isFetching, error } = useQuery({
+
+  const {
+    data,
+    isLoading,
+    isFetching,
+    error,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
     queryKey: ["jobs", agencyId, type],
-    queryFn: () => {
+    queryFn: async ({ pageParam = 1 }) => {
       if (agencyId) {
         return getJobs({
-          page: 1,
+          page: pageParam,
           fetchSize: postedJobs,
           filters: { agencyId: agencyId },
         });
       }
     },
+    getNextPageParam: (lastPage) => {
+      return lastPage?.nextPage ?? false;
+    },
     retry: 3,
+    initialPageParam: 1, 
   });
 
+  const jobs: Job[] = data?.pages.flatMap((page) => page.jobs) ?? [];
+
   if (isLoading || isFetching) {
-    return <Loader text={t('fetching', { type })} />;
+    return <Loader text={t("fetching", { type })} />;
   }
   if (error) {
-    return (
-      <NotFound text={t('fetch_Error', { type })} />
-    );
+    return <NotFound text={t("fetch_Error", { type })} />;
   }
-  if (!data || !data?.jobs || data?.jobs?.length === 0) {
-    return <NotFound text={t('no_jobs', { type })} />;
+  if (jobs.length === 0) {
+    return <NotFound text={t("no_jobs", { type })} />;
   }
 
   const handleJobDetails = (id: string) => {
@@ -65,81 +83,76 @@ const JobCard = ({
 
   return (
     <>
-      <div className={`${agencyStyles.overFlowSection} scroll-box`}>
-        {data.jobs.map((job: any) => {
-          return (
-            <>
-              <Row className={agencyStyles.jobContainer}>
-                <Col md={5} sm={6} className={agencyStyles.jobColumn}>
-                  <Image
-                    src={`${
-                      job.imageUrl
-                        ? `${IMAGE_BASE_URL}/${job.imageUrl}`
-                        : "/no_image.jpg"
-                    }`}
-                    alt="Rectangle"
-                    height={305}
-                    width={800}
-                    blurDataURL="/no_image.jpg"
-                    style={{
-                      width: "100%",
-                      objectFit: "cover",
-                      objectPosition: "top",
-                    }}
-                  />
-                </Col>
-                <Col md={7} sm={6} className={agencyStyles.agencyJobDetailsContainer}>
-                  <div>
-                    <div className={agencyStyles.heading}>
-                      <h3>
-                        {t('jobs_for')} {COUNTRIES[job.location as "bh"].label}{" "}
-                        <span>
-                          {t('approved')} <AiOutlineInfoCircle fontSize={16} />
-                        </span>
-                      </h3>
-                      <ul>
-                        <li>{COUNTRIES[job.location as "bh"].label}</li>
-                      </ul>
-                    </div>
-                    <div className={agencyStyles.facilities}>
-                      <ul>
-                      
-{job.amenities.map((amenity: string, index: number) => (
-
-    <li key={index}>
-      <Image
-        src={FACILITIES_IMAGES[amenity as "Food" | "Transportation" | "Stay" | "Recruitment"]} 
-        alt={t(amenity.toLowerCase())} 
-        width={16}
-        height={16}
-      />{" "}
-      <span>{t(amenity.toLowerCase())}</span> 
-    </li>
-  ))}
-                      </ul>
-                    </div>
+      <InfiniteScroll
+        dataLength={jobs.length}
+        next={fetchNextPage}
+        hasMore={hasNextPage ?? false}
+        loader={<Loader text="Loading jobs..." />}
+      >
+        <div className={`${agencyStyles.overFlowSection} scroll-box`}>
+          {jobs.map((job: Job) => (
+            <Row className={agencyStyles.jobContainer} key={job._id}>
+              <Col md={5} sm={6} className={agencyStyles.jobColumn}>
+                <Image
+                  src={`${job.imageUrl ? `${IMAGE_BASE_URL}/${job.imageUrl}` : "/no_image.jpg"}`}
+                  alt="Rectangle"
+                  height={305}
+                  width={800}
+                  blurDataURL="/no_image.jpg"
+                  style={{
+                    width: "100%",
+                    objectFit: "cover",
+                    objectPosition: "top",
+                  }}
+                />
+              </Col>
+              <Col md={7} sm={6} className={agencyStyles.agencyJobDetailsContainer}>
+                <div>
+                  <div className={agencyStyles.heading}>
+                    <h3>
+                      {t("jobs_for")} {COUNTRIES[job.location as keyof typeof COUNTRIES].label}{" "}
+                      <span>
+                        {t("approved")} <AiOutlineInfoCircle fontSize={16} />
+                      </span>
+                    </h3>
+                    <ul>
+                      <li>{COUNTRIES[job.location as keyof typeof COUNTRIES].label}</li>
+                    </ul>
                   </div>
-                  <Link
-                    href={`/jobs/${job._id}`}
-                    className={`outlined action-buttons ${agencyStyles.button}`}
-                    onClick={() => handleJobDetails(job.jobId)}
-                  >
-                    {t('view_details')}
-                  </Link>
-                </Col>
-              </Row>
-              <hr className={agencyStyles.separator} />
-            </>
-          );
-        })}
-      </div>
+                  <div className={agencyStyles.facilities}>
+                    <ul>
+                      {job.amenities.map((amenity: string, index: number) => (
+                        <li key={index}>
+                          <Image
+                            src={FACILITIES_IMAGES[amenity as keyof typeof FACILITIES_IMAGES]}
+                            alt={t(amenity.toLowerCase())}
+                            width={16}
+                            height={16}
+                          />
+                          <span>{t(amenity.toLowerCase())}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                <Link
+                  href={`/jobs/${job._id}`}
+                  className={`outlined action-buttons ${agencyStyles.button}`}
+                  onClick={() => handleJobDetails(job.jobId)}
+                >
+                  {t("view_details")}
+                </Link>
+              </Col>
+               
+            </Row>
+            
+          ))}
+        </div>
+      </InfiniteScroll>
     </>
   );
 };
 
-export const AgencyJobPostings: React.FC<AgencyJobPostingsType> = ({
-  agencyId,
-  postedJobs,
-}) => {
+export const AgencyJobPostings: React.FC<AgencyJobPostingsType> = ({ agencyId, postedJobs }) => {
   return <JobCard type="active" agencyId={agencyId} postedJobs={postedJobs} />;
 };
