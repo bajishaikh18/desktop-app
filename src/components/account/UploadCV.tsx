3e59b3bca908506950fafd93277625b4efc2a230
Vivek <dev.wonderly@f1studioz.com>
2025-02-12 21:53:21 +0530

    exisitng cv and workvideo details added

--- src/components/account/UploadCV.tsx
+++ src/components/account/UploadCV.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UploadResumeModal from "../auth/UploadResume"; 
 import styles from "../../components/account/SettingsProfile.module.scss";
-
+import { DateTime } from "luxon";
+import { useAuthUserStore } from "@/stores/useAuthUserStore";
 import { Modal } from "react-bootstrap";
 import { useTranslations } from "next-intl";
 import uploadStyles from "../../app/page.module.scss";
@@ -10,12 +11,20 @@ const UploadCV: React.FC = () => {
   const [reset,setReset]= useState(false);
   
   const t = useTranslations("Upload");
+  const tjp = useTranslations("JobApply");
+ const { authUser } = useAuthUserStore();
+ const [resumeDate,setResumeDate]=useState<string|undefined>("");
+ 
+ useEffect(()=>{
+  setResumeDate(authUser?.resume?.uploadDate.toString());
+ },[authUser])
 
   const resetObj = {
     resume: reset
   }
   const onSuccess = ()=>{
-    setReset(true)
+    setReset(true);
+    setResumeDate(new Date().toString() as string)
   }
   return (
     <div className={styles.cardContainer}>
@@ -25,6 +34,24 @@ const UploadCV: React.FC = () => {
       <p className={uploadStyles.modalDescription}>
         {t("you_can_upload_your_cv_to_find_relevant_jobs_and_recommended_jobs_from")}
       </p>
+      {
+        authUser?.resume?.uploadDate &&
+         
+        <div className={styles.option}>
+        <div className={styles.optionContainer}>
+        <div className={styles.optionBody}> <div className={styles.optionHeader}>
+                    <h5>{authUser?.firstName} {authUser?.lastName}.{authUser?.resume?.keyName.split('.').pop()}
+                    </h5>
+
+                    <span className={styles.optionDate}>
+                    {tjp('last_updated')} {DateTime.fromISO(resumeDate as string).toFormat('dd MMM yyyy')}
+                    </span>
+                  </div> 
+                  </div> 
+                  </div>
+                  </div>
+                 // </p>
+}
       <UploadResumeModal
         handleClose={() => {}}  
         onSuccess={onSuccess}    
--- src/components/account/UploadWorkVideo.tsx
+++ src/components/account/UploadWorkVideo.tsx
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UploadResumeModal from "../auth/UploadResume"; 
 import styles from "../../components/account/SettingsProfile.module.scss";
 import uploadStyles from "../../app/page.module.scss";
 import { Modal } from "react-bootstrap";
 import { useTranslations } from "next-intl";
-
+import { DateTime } from "luxon";
+import { useAuthUserStore } from "@/stores/useAuthUserStore";
 
 const UploadWorkVideo: React.FC = () => {
   const [reset,setReset]= useState(false);
   
   const t = useTranslations("Upload");
-
+  const tjp = useTranslations("JobApply");
+ const { authUser } = useAuthUserStore();
+ const [workVideo,setWorkVideo]=useState<string|undefined>("");
+ 
+ useEffect(()=>{
+  setWorkVideo(authUser?.workVideo?.uploadDate.toString());
+ },[authUser])
   const resetObj = {
     video: reset
   }
   const onSuccess = ()=>{
-    setReset(true)
+    setReset(true);
+    
+    setWorkVideo(new Date().toString() as string)
   }
 
   return (
@@ -26,6 +35,24 @@ const UploadWorkVideo: React.FC = () => {
       <p className={uploadStyles.modalDescription}>
         {t("upload_a_video_of_your_work")}
       </p>
+      {
+        authUser?.workVideo?.uploadDate &&
+         
+        <div className={styles.option}>
+        <div className={styles.optionContainer}>
+        <div className={styles.optionBody}> <div className={styles.optionHeader}>
+                    <h5>{authUser?.firstName} {authUser?.lastName}.{authUser?.workVideo?.keyName.split('.').pop()}
+                    </h5>
+
+                    <span className={styles.optionDate}>
+                    {tjp('last_updated')} {DateTime.fromISO(workVideo as string).toFormat('dd MMM yyyy')}
+                    </span>
+                  </div> 
+                  </div> 
+                  </div>
+                  </div>
+                 // </p>
+}
       <UploadResumeModal
         handleClose={() => {}}  
         onSuccess={onSuccess}    
